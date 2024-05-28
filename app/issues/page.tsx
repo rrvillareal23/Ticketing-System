86031a6ec27e3d918248f6f35cf6d2aae7e3e190
Robert (Ricky) Villareal <112342959+rrvillareal23@users.noreply.github.com>
2024-05-28 16:51:44 -0700

    Built new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
 import { themePropDefs } from "@radix-ui/themes/props";
+import Link from "next/link";
 
 const IssuesPage = () => {
   return (
     <div>
-      <Button> New Issue </Button>
+      <Button>
+        <Link href="/issues/new">New Issue </Link>
+      </Button>
     </div>
   );
 };
