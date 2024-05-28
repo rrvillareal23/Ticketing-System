94521800d7aef8f3212c377c2fa0abf0f0688576
Robert (Ricky) Villareal <112342959+rrvillareal23@users.noreply.github.com>
2024-05-28 16:39:02 -0700

    Set up radix-us

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
+import { themePropDefs } from "@radix-ui/themes/props";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button> New Issue </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
